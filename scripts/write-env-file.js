6e9f1a7f3bbf67f64119cc14a1bec02f2bbfa434
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-09-05 09:54:25 +0200

    env file in ci

---
+++ scripts/write-env-file.js
@@ -0,0 +1,46 @@
+const fs = require("fs")
+const path = require("path")
+require("colors")
+
+const root = path.resolve(__dirname, "../")
+
+const doesFileExist = (file) =>
+  new Promise((res, rej) => {
+    fs.access(file, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+      if (err) {
+        console.log(`${file} ${err.code === "ENOENT" ? "does not exist" : "is read-only"}`.yellow)
+        res(false)
+      }
+      res(true)
+    })
+  })
+
+const writeFile = (filename, data) =>
+  new Promise((res, rej) => {
+    fs.writeFile(filename, data, (err) => {
+      if (err) {
+        rej(err)
+      }
+      res()
+    })
+  })
+
+const run = async () => {
+  try {
+    const fileExist = await doesFileExist(path.resolve(root, "./.env"))
+    if (!fileExist) {
+      const env =
+        'DATABASE_URL="file:./db.sqlite"\nFACEBOOK_APP_ID=1234\nFACEBOOK_APP_SECRET=1253123\nGOOGLE_CLIENT_ID=1634234\nGOOGLE_CLIENT_SECRET=some-secret'
+      await writeFile(path.resolve(root, "./.env"), env)
+    }
+    console.log("Successfully completed write env file".green)
+    process.exit(0)
+  } catch (e) {
+    const message = e.message || "Error occured in script".red
+    console.log(message)
+    console.error(e)
+    process.exit(1)
+  }
+}
+
+run()
