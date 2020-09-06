e40950ff8fdba00114c7f73061fbfdd12c855ca3
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-09-06 09:45:08 +0200

    fixed error, included build in github action

--- app/users/queries/getCurrentUser.ts
+++ app/users/queries/getCurrentUser.ts
@@ -1,6 +1,5 @@
 import db from "db"
 import { SessionContext } from "blitz"
-import { sendMessage } from "../../email"
 
 export default async function getCurrentUser(_ = null, ctx: { session?: SessionContext } = {}) {
   if (!ctx.session?.userId) return null
