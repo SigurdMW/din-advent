149a5e72bb17ec5ef6d65407ba4c554896756baf
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-09-08 09:36:53 +0200

    added healthcheck endpoint and added to render.yaml

---
+++ app/api/healthcheck.ts
@@ -0,0 +1,7 @@
+const healthCheck = (req, res) => {
+  res.statusCode = 200
+  res.setHeader("Content-Type", "application/json")
+  res.end(JSON.stringify({ status: "OK" }))
+}
+
+export default healthCheck
