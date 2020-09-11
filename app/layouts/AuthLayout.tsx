0383835587f0280b1018a1f9fba6805097bd463e
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-09-11 15:25:56 +0200

    optimistic auth ui implemnt

--- app/layouts/AuthLayout.tsx
+++ app/layouts/AuthLayout.tsx
@@ -1,12 +1,9 @@
 import Layout from "./Layout"
 import classes from "./ArticleLayout.module.scss"
-import RequireAuth from "./RequireAuth"
 
 export const AuthLayout = ({ title, children }) => (
   <Layout title={title}>
-    <div className={classes.container}>
-      <RequireAuth>{children}</RequireAuth>
-    </div>
+    <div className={classes.container}>{children}</div>
   </Layout>
 )
 
