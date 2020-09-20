fe8c3765dc1ca33e0406f9cc9ad867d52210c0b4
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-09-20 21:07:03 +0200

    wip payment and sharekey

---
+++ app/interfaces/Payment.ts
@@ -0,0 +1,16 @@
+export enum PaymentProvider {
+  stripe = "stripe",
+  vipps = "vipps",
+}
+
+export enum Plan {
+  starter = "starter",
+  basic = "basic",
+  plus = "plus",
+}
+
+export enum PlanPrice {
+  starter = 29,
+  basic = 59,
+  plus = 99,
+}
