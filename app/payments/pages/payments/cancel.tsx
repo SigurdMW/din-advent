156a6e869a4a20275cb413e23ef9a5c1269b7320
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-10-03 09:12:24 +0200

    wip stripe implement Checkout

---
+++ app/payments/pages/payments/cancel.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { BlitzPage, Link } from "blitz"
+import AuthLayout from "app/layouts/AuthLayout"
+
+const PaymentCanceledPage: BlitzPage = () => (
+  <>
+    <h1>Betaling avbrutt</h1>
+    <p>Din betaling ble avbrutt og ingen ting ble belastet.</p>
+    <Link href="/calendars">
+      <a>Gå til dine kalendere</a>
+    </Link>
+  </>
+)
+
+PaymentCanceledPage.getLayout = (page) => (
+  <AuthLayout title="Betalinger - Din Advent">{page}</AuthLayout>
+)
+
+export default PaymentCanceledPage
--- app/payments/pages/payments/new.tsx
+++ app/payments/pages/payments/new.tsx
@@ -1,12 +1,14 @@
 import React from "react"
-import { Link, useRouter, BlitzPage } from "blitz"
+import { Link, BlitzPage } from "blitz"
 import createPayment from "app/payments/mutations/createPayment"
 import PaymentForm from "app/payments/components/PaymentForm"
 import AuthLayout from "app/layouts/AuthLayout"
+import { useStripe } from "../../utils/useStripe"
 
 const NewPaymentPage: BlitzPage = () => {
-  const router = useRouter()
+  const { isReady, Stripe } = useStripe()
 
+  if (!isReady) return <div>Laster....</div>
   return (
     <div>
       <h1>Create New Payment</h1>
@@ -16,9 +18,8 @@ const NewPaymentPage: BlitzPage = () => {
         initialValues={{}}
         onSubmit={async (values) => {
           try {
-            const payment = await createPayment({ data: { plan: values.plan } })
-            alert("Success!" + JSON.stringify(payment))
-            router.push("/payments/[paymentId]", `/payments/${payment.id}`)
+            const id = await createPayment({ data: { plan: values.plan } })
+            Stripe?.redirectToCheckout({ sessionId: id })
           } catch (error) {
             console.error(error)
             alert("Error creating payment " + JSON.stringify(error, null, 2))
