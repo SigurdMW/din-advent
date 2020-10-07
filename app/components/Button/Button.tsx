27db6c2eee3eadacc09e37c288b341e6010afb41
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-10-07 21:14:28 +0200

    MVP share functionality

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import React, { FC, HTMLProps } from "react"
+
+export interface ButtonProps extends Omit<HTMLProps<HTMLButtonElement>, "size"> {
+  type: "primary" | "secondary" | "subtle"
+  size?: "large" | "small"
+  className?: string
+}
+
+export const Button: FC<ButtonProps> = ({ children, type, size, className = "", ...rest }) => {
+  let addedClassName = className ? className + " da-button" : "da-button"
+  if (type === "primary") addedClassName += " da-golden-btn"
+  if (type === "secondary") addedClassName += " da-btn-white"
+  if (type === "subtle") addedClassName += ""
+  if (size === "large") addedClassName += " da-btn-large"
+  return (
+    <button className={addedClassName} {...rest}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
