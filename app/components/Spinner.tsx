d1414a3e0c34e9f514d3c7e8b09ac2b60328c57d
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-10-14 15:54:21 +0200

    allowed to open window?

---
+++ app/components/Spinner.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react"
+
+// Thanks to https://codepen.io/jonathanhooker/pen/wKaXPV
+// Alt: https://codepen.io/chrisgannon/pen/zyOjYw
+
+export const Spinner: FC<{}> = () => {
+  return <div>Laster...</div>
+}
+
+export default Spinner
