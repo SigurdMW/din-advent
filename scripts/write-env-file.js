f3f0b3a05456bb52d996c66ae0ab2bf738825e9a
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-10-21 08:02:45 +0200

    fixing tests e2e

--- scripts/write-env-file.js
+++ scripts/write-env-file.js
@@ -8,6 +8,7 @@ const doesFileExist = (file) =>
   new Promise((res, rej) => {
     fs.access(file, fs.constants.F_OK | fs.constants.W_OK, (err) => {
       if (err) {
+        // eslint-disable-next-line no-console
         console.log(`${file} ${err.code === "ENOENT" ? "does not exist" : "is read-only"}`.yellow)
         res(false)
       }
@@ -29,15 +30,34 @@ const run = async () => {
   try {
     const fileExist = await doesFileExist(path.resolve(root, "./.env"))
     if (!fileExist) {
-      const env =
-        'DATABASE_URL="file:./db.sqlite"\nFACEBOOK_APP_ID=1234\nFACEBOOK_APP_SECRET=1253123\nGOOGLE_CLIENT_ID=1634234\nGOOGLE_CLIENT_SECRET=some-secret\nMAILGUN_API_KEY=somekey\nMAILGUN_DOMAIN=somedomain\nMAILGUN_URL=someurl'
+      const env = `DATABASE_URL="file:./db.sqlite"
+FACEBOOK_APP_ID=1234
+FACEBOOK_APP_SECRET=1253123
+GOOGLE_CLIENT_ID=1634234
+GOOGLE_CLIENT_SECRET=some-secret
+MAILGUN_API_KEY=somekey
+MAILGUN_DOMAIN=somedomain
+MAILGUN_URL=someurl
+VIPPS_CLIENT_ID=as123123
+VIPPS_CLIENT_SECRET=asd123123
+VIPPS_SUBSCRIPTION_KEY=asd1245123
+VIPPS_URL=https://apitest.vipps.no/
+VIPPS_MERCHANT_NUMBER=112353
+STRIPE_KEY=sk_test_asdasdasdsa
+NEXT_PUBLIC_STRIPE_PUBLIC_KEY=pk_test_t3qeasdasdasds
+CLOUDINARY_API_KEY=3333213
+CLOUDINARY_API_SECRET=tqrsadas3243243asdsd
+NEXT_PUBLIC_GA_KEY=""
+NEXT_PUBLIC_RECAPTCHA_KEY=4qrsad1232rsdasd
+RECAPTCHA_SECRET=124rsadasdasd`
       await writeFile(path.resolve(root, "./.env"), env)
     }
+    // eslint-disable-next-line no-console
     console.log("Successfully completed write env file".green)
     process.exit(0)
   } catch (e) {
     const message = e.message || "Error occured in script".red
-    console.log(message)
+    console.error(message)
     console.error(e)
     process.exit(1)
   }
