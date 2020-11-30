1f1bf644c459de2e4887b74164e779171d2a7a48
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-11-30 08:54:44 +0100

    youtube fix

--- app/components/DynamicComponents/YouTubeComponent/YouTubeComponent.tsx
+++ app/components/DynamicComponents/YouTubeComponent/YouTubeComponent.tsx
@@ -29,7 +29,7 @@ export const YouTubeComponent = ({ editorMode, youtubeId, youtubeUrl, onChange }
 				})
 				setEdit(false)
 			} else {
-				setError("Beklager, vi klarte ikke finne hvilken YouTube video du ville legge inn. Lim inn en link som ligner på dette: https://www.youtube.com/watch?v=1gJzzUBt1ys")
+				setError("Beklager, vi klarte ikke finne hvilken YouTube video du ville legge inn. Lim inn en link som ligner på denne: https://www.youtube.com/watch?v=1gJzzUBt1ys")
 			}
 			return
 		}
@@ -44,10 +44,13 @@ export const YouTubeComponent = ({ editorMode, youtubeId, youtubeUrl, onChange }
 						youtubeUrl: toHandle
 					})
 					setEdit(false)
+					return
+				} else {
+					setError("Beklager, vi klarte ikke finne hvilken YouTube video du ville legge inn. Lim inn en link som ligner på denne: https://www.youtube.com/watch?v=1gJzzUBt1ys")
 				}
-			}	
+			}
 		} catch(e) {
-			setError("Beklager, vi klarte ikke finne hvilken YouTube video du ville legge inn. Lim inn en link som ligner på dette: https://www.youtube.com/watch?v=1gJzzUBt1ys")
+			setError("Beklager, vi klarte ikke finne hvilken YouTube video du ville legge inn. Lim inn en link som ligner på denne: https://www.youtube.com/watch?v=1gJzzUBt1ys")
 		}
 	}
 
@@ -58,7 +61,7 @@ export const YouTubeComponent = ({ editorMode, youtubeId, youtubeUrl, onChange }
 					<label>Kopier YouTube-link her:<br/>
 						<input type="text" value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Legg inn link til YouTube video her"/>
 					</label>
-					{error && <><span>{error}</span><br/></>}
+					{error && <><span>{error}</span><br/><br/></>}
 					<Button type="primary" onClick={() => handleYoutubeLink()}>Legg til</Button>{" "}{youtubeUrl && <Button type="secondary" onClick={() => setEdit(false)}>Avbryt</Button>}
 				</div>
 			)
@@ -66,7 +69,7 @@ export const YouTubeComponent = ({ editorMode, youtubeId, youtubeUrl, onChange }
 		return (
 			<div className={classes.root}>
 				Viser YouTube film:<br />
-				<iframe width="560" height="315" title="YouTube film" src={`https://www.youtube.com/embed/${youtubeId}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+				<iframe width="560" height="315" title="YouTube film" src={`https://www.youtube-nocookie.com/embed/${youtubeId}?modestbranding=1&showinfo=0&rel=0&iv_load_policy=3`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 				<br/>
 				<Button type="subtle" onClick={() => setEdit(true)}>Endre</Button>
 			</div>
@@ -75,7 +78,7 @@ export const YouTubeComponent = ({ editorMode, youtubeId, youtubeUrl, onChange }
 	if (!youtubeId) return null
 	return (
 		<div className={classes.root}>
-			<iframe width="560" height="315" title="YouTube film" src={`https://www.youtube.com/embed/${youtubeId}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+			<iframe width="560" height="315" title="YouTube film" src={`https://www.youtube-nocookie.com/embed/${youtubeId}?modestbranding=1&showinfo=0&rel=0&iv_load_policy=3`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 		</div>
 	)
 }
